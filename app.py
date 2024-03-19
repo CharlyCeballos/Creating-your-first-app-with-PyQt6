46f50d3874941177f5cc8c12b17c20fe032c1755
CharlyCeballos <charlyfaceam912@gmail.com>
2024-03-19 09:57:15 -0600

    Storing data 1

--- app.py
+++ app.py
@@ -9,21 +9,24 @@ class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
 
+    self.button_is_checked = True
+
     self.setWindowTitle("My App")
+
     button = QPushButton("Press Me!")
     self.setFixedSize(QSize(400, 300))
+
     button.setCheckable(True)
-    button.clicked.connect(self.the_button_was_clicked)
     button.clicked.connect(self.the_button_was_toggled)
+    button.setChecked(self.button_is_checked)
 
     # Set the central widget of the Window.
     self.setCentralWidget(button)
 
-  def the_button_was_clicked(self):
-    print("Clicked!")
-
   def the_button_was_toggled(self, checked):
-    print("Checked?", checked)
+    self.button_is_checked = checked
+
+    print(self.button_is_checked)
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
