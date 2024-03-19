332355db835a3f06d540704e530e396851efe850
CharlyCeballos <charlyfaceam912@gmail.com>
2024-03-19 10:19:54 -0600

    Changing the interface 1

--- app.py
+++ app.py
@@ -9,23 +9,21 @@ class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
 
-    self.button_is_checked = True
-
     self.setWindowTitle("My App")
 
     self.button = QPushButton("Press Me!")
     self.setFixedSize(QSize(400, 300))
-    self.button.setCheckable(True)
-    self.button.released.connect(self.the_button_was_released)
-    self.button.setChecked(self.button_is_checked)
+    self.button.clicked.connect(self.the_button_was_clicked)
 
     # Set the central widget of the Window.
     self.setCentralWidget(self.button)
 
-  def the_button_was_released(self):
-    self.button_is_checked = self.button.isChecked()
+  def the_button_was_clicked(self):
+    self.button.setText("You already clicked me.")
+    self.button.setEnabled(False)
 
-    print(self.button_is_checked)
+    # Also change the window title.
+    self.setWindowTitle("My Oneshot App")
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
