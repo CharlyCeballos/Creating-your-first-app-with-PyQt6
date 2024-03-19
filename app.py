c3fbdf80eac9c7979e624a81a4862f8ce55d151b
CharlyCeballos <charlyfaceam912@gmail.com>
2024-03-19 12:43:59 -0600

    Connecting widgets together directly

--- app.py
+++ app.py
@@ -1,54 +1,30 @@
 # Only needed for access to command line arguments
 import sys
 
-from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
-from random import choice
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
 
-window_titles = [
-  'My App',
-  'My App',
-  'Still My App',
-  'Still My App',
-  'What on earth',
-  'What on earth',
-  'This is surprising',
-  'This is surprising',
-  'Something went wrong'
-]
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
 
-    self.n_times_clicked = 0
-
     self.setWindowTitle("My App")
 
-    self.button = QPushButton("Press Me!")
-    self.setFixedSize(QSize(400, 300))
-    self.button.clicked.connect(self.the_button_was_clicked)
+    self.label = QLabel()
 
-    self.windowTitleChanged.connect(self.the_window_title_changed)
+    self.input = QLineEdit()
+    self.input.textChanged.connect(self.label.setText)
+
+    layout = QVBoxLayout()
+    layout.addWidget(self.input)
+    layout.addWidget(self.label)
+
+    container = QWidget()
+    container.setLayout(layout)
 
     # Set the central widget of the Window.
-    self.setCentralWidget(self.button)
-
-  def the_button_was_clicked(self):
-    self.n_times_clicked = self.n_times_clicked + 1
-    print(f"Clicked {self.n_times_clicked} times.")
-    new_window_title = choice(window_titles)
-    print("Setting title:  %s" % new_window_title)
-
-    # Also change the window title.
-    self.setWindowTitle(new_window_title)
-
-  def the_window_title_changed(self, window_title):
-    print("Window title changed: %s" % window_title)
-
-    if window_title == 'Something went wrong':
-      self.button.setDisabled(True)
+    self.setCentralWidget(container)
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
