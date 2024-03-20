720802a8b2e26583894596893fad27dee4fb1796
CharlyCeballos <charlyfaceam912@gmail.com>
2024-03-19 18:06:00 -0600

    Mouse events

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Only needed for access to command line arguments
 import sys
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
 
 
@@ -19,13 +20,37 @@ class MainWindow(QMainWindow):
     self.label.setText("mouseMoveEvent")
 
   def mousePressEvent(self, e):
-    self.label.setText("mousePressEvent")
+    if e.button() == Qt.MouseButton.LeftButton:
+      # handle the left-button press in here
+      self.label.setText("mousePressEvent LEFT")
+
+    elif e.button() == Qt.MouseButton.MiddleButton:
+      # handle the middle-button press in here.
+      self.label.setText("mousePressEvent MIDDLE")
+
+    elif e.button() == Qt.MouseButton.RightButton:
+      # handle the right-button press in here.
+      self.label.setText("mousePressEvent RIGHT")
 
   def mouseReleaseEvent(self, e):
-    self.label.setText("mouseReleaseEvent")
+    if e.button() == Qt.MouseButton.LeftButton:
+      self.label.setText("mouseReleaseEvent LEFT")
+
+    elif e.button() == Qt.MouseButton.MiddleButton:
+      self.label.setText("mouseReleaseEvent MIDDLE")
+
+    elif e.button() == Qt.MouseButton.RightButton:
+      self.label.setText("mouseReleaseEvent RIGHT")
 
   def mouseDoubleClickEvent(self, e):
-    self.label.setText("mouseDoubleClickEvent")
+    if e.button() == Qt.MouseButton.LeftButton:
+      self.label.setText("mouseDoubleClickEvent LEFT")
+
+    elif e.button() == Qt.MouseButton.MiddleButton:
+      self.label.setText("mouseDoubleClickEvent MIDDLE")
+
+    elif e.button() == Qt.MouseButton.RightButton:
+      self.label.setText("mouseDoubleClickEvent RIGHT")
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
