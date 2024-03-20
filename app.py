045a81d6764b0c13ad77a025002ef81b46d2b13e
CharlyCeballos <charlyfaceam912@gmail.com>
2024-03-19 18:17:38 -0600

    Context menus 1

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 import sys
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QMenu
 
 
 # Subclass QMainWindow to customize your application's main window
@@ -11,46 +12,17 @@ class MainWindow(QMainWindow):
     super().__init__()
 
     self.setWindowTitle("My App")
-    self.label = QLabel("Click in this window")
+    self.label = QLabel("Test right button please")
 
     # Set the central widget of the Window.
     self.setCentralWidget(self.label)
 
-  def mouseMoveEvent(self, e):
-    self.label.setText("mouseMoveEvent")
-
-  def mousePressEvent(self, e):
-    if e.button() == Qt.MouseButton.LeftButton:
-      # handle the left-button press in here
-      self.label.setText("mousePressEvent LEFT")
-
-    elif e.button() == Qt.MouseButton.MiddleButton:
-      # handle the middle-button press in here.
-      self.label.setText("mousePressEvent MIDDLE")
-
-    elif e.button() == Qt.MouseButton.RightButton:
-      # handle the right-button press in here.
-      self.label.setText("mousePressEvent RIGHT")
-
-  def mouseReleaseEvent(self, e):
-    if e.button() == Qt.MouseButton.LeftButton:
-      self.label.setText("mouseReleaseEvent LEFT")
-
-    elif e.button() == Qt.MouseButton.MiddleButton:
-      self.label.setText("mouseReleaseEvent MIDDLE")
-
-    elif e.button() == Qt.MouseButton.RightButton:
-      self.label.setText("mouseReleaseEvent RIGHT")
-
-  def mouseDoubleClickEvent(self, e):
-    if e.button() == Qt.MouseButton.LeftButton:
-      self.label.setText("mouseDoubleClickEvent LEFT")
-
-    elif e.button() == Qt.MouseButton.MiddleButton:
-      self.label.setText("mouseDoubleClickEvent MIDDLE")
-
-    elif e.button() == Qt.MouseButton.RightButton:
-      self.label.setText("mouseDoubleClickEvent RIGHT")
+  def contextMenuEvent(self, e):
+    context = QMenu(self)
+    context.addAction(QAction("test 1", self))
+    context.addAction(QAction("test 2", self))
+    context.addAction(QAction("test 3", self))
+    context.exec(e.globalPos())
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
