a7df4430f26ca5caf2290d1f2ab130fca3c791d6
CharlyCeballos <charlyfaceam912@gmail.com>
2024-03-22 14:35:43 -0600

    C) QLabel

---
+++ c_18.py
@@ -0,0 +1,40 @@
+# Only needed for access to command line arguments
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+  QMainWindow,
+  QApplication,
+  QLabel,
+)
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+
+  def __init__(self):
+    super(MainWindow, self).__init__()
+
+    self.setWindowTitle("My App")
+
+    widget = QLabel("Hello")
+    font = widget.font()
+    font.setPointSize(30)
+    widget.setFont(font)
+    widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+
+    self.setCentralWidget(widget)
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
+window = MainWindow()
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec()
+
+
+# Your application won't reach here until you exit and the event loop has stopped.
