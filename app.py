53ea8b533d5dbcf599c0971ccaf4729ee685072f
CharlyCeballos <charlyfaceam912@gmail.com>
2024-03-22 14:34:58 -0600

    C) QLabel

--- app.py
+++ app.py
@@ -3,65 +3,26 @@ import sys
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
-  QApplication,
-  QCheckBox,
-  QComboBox,
-  QDateEdit,
-  QDateTimeEdit,
-  QDial,
-  QDoubleSpinBox,
-  QFontComboBox,
-  QLabel,
-  QLCDNumber,
-  QLineEdit,
   QMainWindow,
-  QProgressBar,
-  QPushButton,
-  QRadioButton,
-  QSlider,
-  QSpinBox,
-  QTimeEdit,
-  QVBoxLayout,
-  QWidget,
+  QApplication,
+  QLabel,
 )
 
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
+
   def __init__(self):
-    super().__init__()
-    self.show()
+    super(MainWindow, self).__init__()
 
-    self.setWindowTitle("Widgets App")
+    self.setWindowTitle("My App")
 
-    layout = QVBoxLayout()
-    widgets = [
-      QCheckBox,
-      QComboBox,
-      QDateEdit,
-      QDateTimeEdit,
-      QDial,
-      QDoubleSpinBox,
-      QFontComboBox,
-      QLCDNumber,
-      QLabel,
-      QLineEdit,
-      QProgressBar,
-      QPushButton,
-      QRadioButton,
-      QSlider,
-      QSpinBox,
-      QTimeEdit,
-    ]
+    widget = QLabel("Hello")
+    font = widget.font()
+    font.setPointSize(30)
+    widget.setFont(font)
+    widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
 
-    for w in widgets:
-      layout.addWidget(w())
-
-    widget = QWidget()
-    widget.setLayout(layout)
-
-    # Set the central widget of the Window. Widget will expand
-    # to take up all the space in the window by default.
     self.setCentralWidget(widget)
 
 # You need one (and only one) QApplication instance per application.
@@ -78,25 +39,3 @@ app.exec()
 
 
 # Your application won't reach here until you exit and the event loop has stopped.
-###################################################################################
-# Lets have a look at all the example widgets, from top to bottom:
-'''
-Widget 	        ||  What it does
-____________________________________
-QCheckbox 	    ||  A checkbox
-QComboBox 	    ||  A dropdown list box
-QDateEdit 	    ||  For editing dates and datetimes
-QDateTimeEdit 	||  For editing dates and datetimes
-QDial 	        ||  Rotatable dial
-QDoubleSpinBox 	||  A number spinner for floats
-QFontComboBox 	||  A list of fonts
-QLCDNumber 	    ||  A quite ugly LCD display
-QLabel 	        ||  Just a label, not interactive
-QLineEdit 	    ||  Enter a line of text
-QProgressBar 	  ||  A progress bar
-QPushButton 	  ||  A button
-QRadioButton 	  ||  A toggle set, with only one active item
-QSlider 	      ||  A slider
-QSpinBox 	      ||  An integer spinner
-QTimeEdit 	    ||  For editing times
-'''
